94e16d0414e63fc2d4fef0e3a84ef1bce5612a1f
Rasyid Rafi <muhammand.rafi25@gmail.com>
2021-03-08 12:07:47 +0700

    Fix Interval

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
-const seconds = 10; // How many seconds interval
-const site = ["https://www.google.com/", "https://duckduckgo.com/"];
+const seconds = 120; // How many seconds interval
+const site = [
+  "https://www.google.com/",
+  "https://duckduckgo.com/",
+  "othersite",
+];
 
 module.exports = {
   site,
